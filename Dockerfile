e7ff1355911c0499b1c5555fbf4c1d05118d276f
burny <gamingburny@gmail.com>
2022-05-24 15:54:03 +0200

    Make Dockerfile more readable again and use --squash flag to reduce image size

--- Dockerfile
+++ Dockerfile
@@ -2,41 +2,51 @@
 ARG PYTHON_VERSION=3.9
 ARG SC2_VERSION=4.10
 ARG VERSION_NUMBER=1.0.0
+ARG USE_SQUASHED
 
-FROM aiarena/sc2-linux-base:py_$PYTHON_VERSION-sc2_$SC2_VERSION-v$VERSION_NUMBER
+FROM aiarena/sc2-linux-base:py_$PYTHON_VERSION-sc2_$SC2_VERSION-v$VERSION_NUMBER$USE_SQUASHED
 MAINTAINER AI Arena <staff@aiarena.net>
+
+WORKDIR /root/
 
 # Prevent caching unless client master branch changed
 # https://codehunter.cc/a/git/how-to-prevent-dockerfile-caching-git-clone
 ADD https://api.github.com/repos/aiarena/aiarena-client/git/refs/heads/master version.json
+RUN rm version.json
 
 # Download python requirements files
 ADD https://raw.githubusercontent.com/aiarena/aiarena-client/master/requirements.txt client-requirements.txt
 
 # Add local pyproject.toml and poetry.lock which contain bot requirements
-ADD pyproject.toml poetry.lock ./
+COPY pyproject.toml poetry.lock ./
 
 # Merge client and bot requirements into pyproject.toml, generate a requirements.txt and install the packages globally
-RUN pip install poetry \
-    # Allows the final remove virtual env command
-    && poetry config virtualenvs.in-project true \
-    # Merge client requirements into current requirements
-    && poetry add $(cat client-requirements.txt) \
-    # Export unified requirements as requirements.txt, this will not include dev dependencies
-    && poetry export -f requirements.txt --output requirements.txt --without-hashes \
-    # Remove virtual environment
-    && pip uninstall -y poetry \
-    && rm -rf .venv \
-    # Install requirements.txt globally
-    && pip install -r requirements.txt
+RUN pip install poetry
+# Allows the final remove virtual env command
+RUN poetry config virtualenvs.in-project true
+# Merge client requirements into current requirements
+RUN poetry add $(cat client-requirements.txt)
+# Export unified requirements as requirements.txt, this will not include dev dependencies
+RUN poetry export -f requirements.txt --output requirements.txt --without-hashes
+# Remove virtual environment
+RUN pip uninstall -y poetry
+RUN rm -rf .venv
+# Install requirements.txt globally
+RUN pip install -r requirements.txt
 
-# Download the aiarena client
-RUN git clone https://github.com/aiarena/aiarena-client.git aiarena-client \
-    # Create bot users
-    && useradd -ms /bin/bash bot_player1 \
-    && useradd -ms /bin/bash bot_player2 \
-    # Create Bot and Replay directories
-    && mkdir -p /root/aiarena-client/Bots \
+# Download the aiarena client to /root/aiarena-client
+# https://stackoverflow.com/a/3946745/10882657
+RUN wget --quiet --show-progress --progress=bar:force https://github.com/aiarena/aiarena-client/archive/refs/heads/master.zip \
+    && unzip -q master.zip \
+    && mv aiarena-client-master aiarena-client \
+    && rm master.zip
+
+# Create bot users
+RUN useradd -ms /bin/bash bot_player1 \
+    && useradd -ms /bin/bash bot_player2
+
+# Create Bot and Replay directories
+RUN mkdir -p /root/aiarena-client/Bots \
     && mkdir -p /root/aiarena-client/Replays
 
 # Change to working directory
--- test_docker_image.sh
+++ test_docker_image.sh
@@ -1,33 +1,54 @@
 # This script is meant for development, which produces fresh images and then runs tests
 
 # Set which versions to use
-export PYTHON_VERSION=3.9
-export SC2_VERSION=4.10
-export VERSION_NUMBER=1.0.0
+export VERSION_NUMBER=${VERSION_NUMBER:-1.0.0}
+export PYTHON_VERSION=${PYTHON_VERSION:-3.9}
+export SC2_VERSION=${SC2_VERSION:-4.10}
 
+# Allow image squashing by enabling experimental docker features
+# https://stackoverflow.com/a/21164441/10882657
+# https://github.com/actions/virtual-environments/issues/368#issuecomment-582387669
+file=/etc/docker/daemon.json
+if [ ! -e "$file" ]; then
+  echo $'{\n    "experimental": true\n}' | sudo tee /etc/docker/daemon.json
+  sudo systemctl restart docker.service
+fi
+
+# SC2LINUXBASE
 # Build images
-docker build -f sc2linuxbase-docker/Dockerfile -t aiarena/sc2-linux-base:py_$PYTHON_VERSION-sc2_$SC2_VERSION-v$VERSION_NUMBER --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg SC2_VERSION=$SC2_VERSION .
-docker build -t aiarena/arenaclient:latest --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg SC2_VERSION=$SC2_VERSION --build-arg VERSION_NUMBER=$VERSION_NUMBER .
+docker build -t aiarena/sc2-linux-base:py_$PYTHON_VERSION-sc2_$SC2_VERSION-v$VERSION_NUMBER --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg SC2_VERSION=$SC2_VERSION - < sc2linuxbase-docker/Dockerfile
+# Squash image
+#docker build -t aiarena/sc2-linux-base:py_$PYTHON_VERSION-sc2_$SC2_VERSION-v$VERSION_NUMBER-squashed --squash --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg SC2_VERSION=$SC2_VERSION - < sc2linuxbase-docker/Dockerfile
+
+# ARENACLIENT
+docker build -t aiarena/arenaclient:local --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg SC2_VERSION=$SC2_VERSION --build-arg VERSION_NUMBER=$VERSION_NUMBER .
+# Squash image
+#docker build -t aiarena/arenaclient:local-squashed --squash --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg SC2_VERSION=$SC2_VERSION --build-arg VERSION_NUMBER=$VERSION_NUMBER .
+
+# Build arenaclient from squashed base image
+#docker build -t aiarena/arenaclient:local --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg SC2_VERSION=$SC2_VERSION --build-arg VERSION_NUMBER=$VERSION_NUMBER --build-arg USE_SQUASHED="-squashed" .
+# Squash image
+#docker build -t aiarena/arenaclient:local-squashed --squash --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg SC2_VERSION=$SC2_VERSION --build-arg VERSION_NUMBER=$VERSION_NUMBER --build-arg USE_SQUASHED="-squashed" .
 
 # Delete previous container if it exists
 docker rm -f testcontainer
 
 # Start container with correct env variables, override the default entrypoint
-docker run -it -d \
+docker run -i -d \
   --name testcontainer \
   --env SC2_PROXY_BIN=SC2_x64 \
   --env SC2_PROXY_BASE=/root/StarCraftII/ \
   --entrypoint /bin/bash \
   --workdir="/root/aiarena-client" \
-  aiarena/arenaclient:latest
+  aiarena/arenaclient:local
 
 # Add maps
-docker exec -it testcontainer bash -c "cp -R /root/aiarena-client/testing/maps/* /root/StarCraftII/maps"
+docker exec -i testcontainer bash -c "cp -R /root/aiarena-client/testing/maps/* /root/StarCraftII/maps"
 # Add bots
-docker exec -it testcontainer bash -c "git clone https://github.com/aiarena/aiarena-test-bots testing/aiarena-test-bots"
+docker exec -i testcontainer bash -c "git clone https://github.com/aiarena/aiarena-test-bots testing/aiarena-test-bots"
 
 # Run integration tests, this may take around 15 minutes
-docker exec -it testcontainer bash -c "python -m arenaclient --test"
+docker exec -i testcontainer bash -c "python -m arenaclient --test"
 
 # Command for entering the container to debug if something went wrong:
 # docker exec -it testcontainer bash
